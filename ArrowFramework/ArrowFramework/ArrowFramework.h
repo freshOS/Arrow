3e8f3fae83ec79e472749221ee9a5ca9a648b8ae
s4cha <sachadso@gmail.com>
2015-06-07 18:03:09 +0200

    Share Framework scheme for carthage

--- ArrowFramework/ArrowFramework/ArrowFramework.h
+++ ArrowFramework/ArrowFramework/ArrowFramework.h
@@ -14,6 +14,4 @@ FOUNDATION_EXPORT double ArrowFrameworkVersionNumber;
 //! Project version string for ArrowFramework.
 FOUNDATION_EXPORT const unsigned char ArrowFrameworkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ArrowFramework/PublicHeader.h>
-
 
