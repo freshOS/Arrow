ea487ae7c0289e8e8592e025ae4482f42852fac3
S4cha <sachadso@gmail.com>
2018-06-26 18:33:01 +0200

    WIP - init(json:)

--- Source/Extensions.swift
+++ Source/Extensions.swift
@@ -14,18 +14,19 @@ public protocol ArrowInitializable {
 
 // MARK: - Plain types
 
-//extension String: ArrowInitializable {
-//    /// Construct a `String` from JSON
-//    public init?(_ json: JSON) {
-//        var x: String?
-//        x <-- json
-//        guard let s = x else {
-//            return nil
-//        }
-//        self.init(s)
-//    }
-//}
-//
+extension String: ArrowInitializable {
+    /// Construct a `String` from JSON
+    public init(_ json: JSON) {
+        var x: String?
+        x <-- json
+        if let s = x {
+            self.init(s)
+        } else {
+            self.init()
+        }
+    }
+}
+
 extension Int: ArrowInitializable {
     /// Construct an `Int` from JSON
     public init(_ json: JSON) {
@@ -38,132 +39,145 @@ extension Int: ArrowInitializable {
         }
     }
 }
-//
-//extension UInt: ArrowInitializable {
-//    /// Construct a `UInt` from JSON
-//    public init?(_ json: JSON) {
-//        var x: UInt?
-//        x <-- json
-//        guard let u = x else {
-//            return nil
-//        }
-//        self.init(u)
-//    }
-//}
-//
-//extension Double: ArrowInitializable {
-//    /// Construct a `Double` from JSON
-//    public init?(_ json: JSON) {
-//        var x: Double?
-//        x <-- json
-//        guard let d = x else {
-//            return nil
-//        }
-//        self.init(d)
-//    }
-//}
-//
-//extension Float: ArrowInitializable {
-//    /// Construct a `Float` from JSON
-//    public init?(_ json: JSON) {
-//        var x: Float?
-//        x <-- json
-//        guard let f = x else {
-//            return nil
-//        }
-//        self.init(f)
-//    }
-//}
-//
-//extension CGFloat: ArrowInitializable {
-//    /// Construct a `CGFloat` from JSON
-//    public init?(_ json: JSON) {
-//        var x: CGFloat?
-//        x <-- json
-//        guard let f = x else {
-//            return nil
-//        }
-//        self.init(f)
-//    }
-//}
-//
-//extension Bool: ArrowInitializable {
-//    /// Construct a `Bool` from JSON
-//    public init?(_ json: JSON) {
-//        var x: Bool?
-//        x <-- json
-//        guard let b = x else {
-//            return nil
-//        }
-//        self.init(b)
-//    }
-//}
-//
-//extension URL: ArrowInitializable {
-//    /// Construct a `URL` from JSON
-//    public init?(_ json: JSON) {
-//        var x: String?
-//        x <-- json
-//        guard let s = x else {
-//            return nil
-//        }
-//        self.init(string: s)
-//    }
-//}
-//
-//// MARK: - Raw representable
-//
+
+extension UInt: ArrowInitializable {
+    /// Construct a `UInt` from JSON
+    public init(_ json: JSON) {
+        var x: UInt?
+        x <-- json
+        if let u = x {
+            self.init(u)
+        } else {
+            self.init()
+        }
+    }
+}
+
+extension Double: ArrowInitializable {
+    /// Construct a `Double` from JSON
+    public init(_ json: JSON) {
+        var x: Double?
+        x <-- json
+        if let d = x {
+            self.init(d)
+        } else {
+            self.init()
+        }
+    }
+}
+
+extension Float: ArrowInitializable {
+    /// Construct a `Float` from JSON
+    public init(_ json: JSON) {
+        var x: Float?
+        x <-- json
+        if let f = x {
+            self.init(f)
+        } else {
+            self.init()
+        }
+    }
+}
+
+extension CGFloat: ArrowInitializable {
+    /// Construct a `CGFloat` from JSON
+    public init(_ json: JSON) {
+        var x: CGFloat?
+        x <-- json
+        if let f = x {
+            self.init(f)
+        } else {
+            self.init()
+        }
+    }
+}
+
+extension Bool: ArrowInitializable {
+    /// Construct a `Bool` from JSON
+    public init(_ json: JSON) {
+        var x: Bool?
+        x <-- json
+        if let b = x {
+            self.init(b)
+        } else {
+            self.init()
+        }
+    }
+}
+
+extension URL: ArrowInitializable {
+    /// Construct a `URL` from JSON
+    public init(_ json: JSON) {
+        var x: String?
+        x <-- json
+        if let x = x, let _ = URL(string: x) {
+            self.init(string: x)!
+        } else {
+            self.init(string: "https//")!
+        }
+    }
+}
+
+// MARK: - Raw representable
+
 //extension RawRepresentable where RawValue: ArrowInitializable {
 //    /// Construct a `RawRepresentable` from JSON
-//    public init?(_ json: JSON) {
+//    public init(_ json: JSON) {
 //        var x: Self.RawValue?
+//
 //        x <-- json
-//        guard let r = x else {
-//            return nil
-//        }
-//        self.init(rawValue: r)
-//    }
-//}
+//        if let r = x {
+//            self.init(rawValue: r)!
+//        } else {
+////            self.init(rawValue: r)!
 //
-//// MARK: - Arrays
-//
-//extension Array where Element: ArrowInitializable {
-//    /// Construct an `Array` from JSON
-//    public init?(_ json: JSON) {
-//        var x: [Element]?
-//        x <-- json
-//        guard let a = x else {
-//            return nil
-//        }
-//        self.init(a)
-//    }
-//}
-//
-//extension Array where Element: RawRepresentable, Element.RawValue: ArrowInitializable {
-//    /// Construct an `Array` from JSON
-//    public init?(_ json: JSON) {
-//        var x: [Element]?
-//        x <-- json
-//        guard let a = x else {
-//            return nil
-//        }
-//        self.init(a)
-//    }
-//}
-//
-//// MARK: - Dictionaries
-//
-//extension Dictionary where Value: ArrowInitializable {
-//    /// Construct a `Dictionary` from JSON
-//    public init?(_ json: JSON) {
-//        var x: [Key: Value]?
-//        x <-- json
-//        guard let d = x else {
-//            return nil
-//        }
-//        self.init()
-//        for (k, v) in d {
-//            self.updateValue(v, forKey: k)
+//            self.init(rawValue:"")
 //        }
 //    }
 //}
+
+// MARK: - Arrays
+
+extension Array where Element: ArrowInitializable {
+    /// Construct an `Array` from JSON
+    public init(_ json: JSON) {
+        var x: [Element]?
+        x <-- json
+        if let a = x {
+            self.init(a)
+        } else {
+            self.init()
+        }
+    }
+}
+
+extension Array where Element: RawRepresentable, Element.RawValue: ArrowInitializable {
+    /// Construct an `Array` from JSON
+    public init(_ json: JSON) {
+        var x: [Element]?
+        x <-- json
+        if let a = x {
+            self.init(a)
+        } else {
+            self.init()
+        }
+    }
+}
+
+// MARK: - Dictionaries
+
+extension Dictionary where Value: ArrowInitializable {
+    /// Construct a `Dictionary` from JSON
+    public init(_ json: JSON) {
+        var x: [Key: Value]?
+        x <-- json
+        if let d = x {
+            self.init()
+            for (k, v) in d {
+                self.updateValue(v, forKey: k)
+            }
+        } else {
+            self.init()
+        }
+    }
+}
