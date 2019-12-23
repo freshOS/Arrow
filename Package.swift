ede0996c0b8f32a67bba80ae384f0898e9a8cb95
S4cha <sachadso@gmail.com>
2019-12-23 17:30:13 +0100

    Updates Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-  name: "Arrow"
+    name: "Arrow",
+    platforms: [.iOS(.v8)],
+    products: [.library(name: "Arrow", targets: ["Arrow"])],
+    targets: [
+        .target(name: "Arrow", path: "Source"),
+        .testTarget(name: "ArrowTests", dependencies: ["Arrow"], path: "ArrowTests")
+    ]
 )
